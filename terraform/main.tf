d3d552afb441614ec3c046c73cfe5bb71bd21ec9
Chafah Sani <forchu.cha@gmail.com>
2026-07-10 02:22:55 +0200

    Add EBS CSI driver addon with IRSA, restore Prometheus persistent storage

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,9 +59,13 @@ module "eks" {
 
   # EKS Addons
   cluster_addons = {
-    coredns    = { most_recent = true }
-    kube-proxy = { most_recent = true }
-    vpc-cni    = { most_recent = true }
+    coredns            = { most_recent = true }
+    kube-proxy         = { most_recent = true }
+    vpc-cni            = { most_recent = true }
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_irsa.iam_role_arn
+    }
   }
 
   # Managed Node Group with ASG
@@ -72,6 +76,27 @@ module "eks" {
       desired_size   = var.node_desired_size
       min_size       = var.node_min_size
       max_size       = var.node_max_size
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+# EBS CSI Driver IRSA
+module "ebs_csi_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "5.44.0"
+
+  role_name = "${var.cluster_name}-${var.environment}-ebs-csi"
+
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
     }
   }
 
